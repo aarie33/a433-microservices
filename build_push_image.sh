7f12165315dceefd83b70cf28099ff86da2a4e31
Ahmad Syamsul Arifin <aarie33ahmad@gmail.com>
2024-06-26 10:01:58 +0700

    dockerize and rabbitmq config

---
+++ build_push_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set variables
+DOCKER_USERNAME=aarie33
+IMAGE_NAME=order-service
+TAG=latest
+
+# Build Docker image
+docker build -t $DOCKER_USERNAME/$IMAGE_NAME:$TAG .
+
+# Login to Docker Hub
+echo $PASSWORD_DOCKER_HUB | docker login -u $DOCKER_USERNAME --password-stdin
+
+# Push Docker image
+docker push $DOCKER_USERNAME/$IMAGE_NAME:$TAG
+
